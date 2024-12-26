8044a0adfcbf9fabc158eaa77f1f9ac8dbb13d3a
cadet-kim <164817837+cadet-kim@users.noreply.github.com>
2024-12-27 01:07:57 +0900

    Update synch.c
    주어진 스레드와 락에 대해 우선순위 상속(priority donation) 메커니즘을 구현한 코드

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -196,6 +196,14 @@ lock_acquire (struct lock *lock)
   ASSERT (!intr_context ());
   ASSERT (!lock_held_by_current_thread (lock));
 
+  struct thread *cur = thread_current ();
+  if (lock->holder) {
+     cur->wait_on_lock = lock;
+     list_insert_ordered (&lock->holder->donations, &cur->donation_elem, 
+    			thread_compare_donate_priority, 0);
+     donate_priority ();
+  }
+
   sema_down (&lock->semaphore);
   lock->holder = thread_current ();
 }
