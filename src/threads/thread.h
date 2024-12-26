39a751b48e696a44ed76e1d201bda7aa56a2dd4a
cadet-kim <164817837+cadet-kim@users.noreply.github.com>
2024-12-27 01:25:12 +0900

    Update thread.h
    .

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -146,6 +146,8 @@ void thread_awake(int64_t ticks);
 //새롭게 sleep,awake 함수를 추가하였으므로 thread.h에 프로토타입을 선언해주어야함.
 
 #endif /* threads/thread.h */
+bool thread_compare_priority(const struct list_elem *a, const struct list_elem *b, void *aux);
+
 void update_next_tick_to_awake(int64_t ticks);
 int64_t get_next_tick_to_awake(void);
 void thread_test_preemption(void);
