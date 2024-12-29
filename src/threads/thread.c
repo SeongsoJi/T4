d9350b36caf583fb142260edfb670a1ca51ff433
cadet-kim <164817837+cadet-kim@users.noreply.github.com>
2024-12-30 02:05:51 +0900

    Update thread.c

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -353,11 +353,7 @@ thread_foreach (thread_action_func *func, void *aux)
 
 /* Sets the current thread's priority to NEW_PRIORITY. */
 void
-thread_set_priority (int new_priority) 
-{
-  thread_current ()->priority = new_priority;
-  thread_test_preemption ();
-}
+
 
 /* Returns the current thread's priority. */
 int
