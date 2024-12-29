34b64c8d3905b0637e371c06e59945f7989b16a7
cadet-kim <164817837+cadet-kim@users.noreply.github.com>
2024-12-30 01:52:11 +0900

    Update thread.h

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -88,7 +88,11 @@ struct thread
     char name[16];                      /* Name (for debugging purposes). */
     uint8_t *stack;                     /* Saved stack pointer. */
     int priority;                       /* Priority. */
-    struct list_elem allelem;           /* List element for all threads list. */
+    struct list_elem allelem;/* List element for all threads list. */
+    int init_priority;
+    struct lock *wait_on_lock;
+    struct list donations;
+    struct list_elem donation_elem;
 
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;  /* List element. */
