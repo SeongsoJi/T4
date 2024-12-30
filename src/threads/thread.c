e4f0ac36fc4bb34ed7966440bf5e69bc9a130716
cadet-kim <164817837+cadet-kim@users.noreply.github.com>
2024-12-30 09:00:31 +0900

    Update thread.c
    s

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -98,7 +98,7 @@ init_thread (struct thread *t, const char *name, int priority) {
     memset (t, 0, sizeof *t);
     t->status = THREAD_BLOCKED;
     strlcpy (t->name, name, sizeof t->name);
-    t->tf.esp = (uint64_t) t + PGSIZE - sizeof (void *);
+    t->tf.esp = (uint32_t)t + PGSIZE - sizeof(void *);
     t->priority = priority;
     t->magic = THREAD_MAGIC;
     // priority donation
